6574685dff784a69793d8163334e7b1b447e07af
Angel Martin Antezana Flores <ghehahanghel@gmail.com>
2022-07-14 22:57:01 -0400

    Configuracion del template

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,36 @@
 export const environment = {
     production: true,
-    hmr       : false
+    hmr       : false,
+    defaultLayoutConfig: {
+        colorTheme      : 'theme-default',
+        customScrollbars: true,
+        layout          : {
+            style    : 'vertical-layout-1',
+            width    : 'fullwidth',
+            navbar   : {
+                primaryBackground  : 'fuse-navy-700',
+                secondaryBackground: 'fuse-navy-900',
+                folded             : false,
+                hidden             : true,
+                position           : 'left',
+                variant            : 'vertical-style-1'
+            },
+            toolbar  : {
+                customBackgroundColor: false,
+                background           : 'fuse-white-500',
+                hidden               : true,
+                position             : 'below-static'
+            },
+            footer   : {
+                customBackgroundColor: true,
+                background           : 'fuse-navy-900',
+                hidden               : true,
+                position             : 'below-fixed'
+            },
+            sidepanel: {
+                hidden  : true,
+                position: 'right'
+            }
+        }
+    }
 };
